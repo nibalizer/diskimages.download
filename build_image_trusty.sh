2a0b6539cb23c6bc0e8f5f526bd5073f74015494
Spencer Krum <nibz@spencerkrum.com>
2016-08-29 21:47:42 -0700

    Ubuntu is not trusty, again

--- build_image_trusty.sh
+++ build_image_trusty.sh
@@ -7,5 +7,5 @@ sudo ls # just to make sure sudo is hot
 
 disk-image-create vm centos -a amd64 -o centos6_x64
 
-mv /var/www/html/diskimages/ubuntu_x64.qcow2 /var/www/html/diskimages/ubuntu_x64_1.qcow2
-mv ubuntu_x64.qcow2 /var/www/html/diskimages/
+mv /var/www/html/diskimages/trusty_x64.qcow2 /var/www/html/diskimages/trusty_x64_1.qcow2
+mv trusty_x64.qcow2 /var/www/html/diskimages/
