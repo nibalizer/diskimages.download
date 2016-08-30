c160d2a89924f95fcbd8d39197b7682f1bdd5ed4
Spencer Krum <nibz@spencerkrum.com>
2016-08-29 21:53:42 -0700

    Seriously, trusty

--- build_image_trusty.sh
+++ build_image_trusty.sh
@@ -5,7 +5,7 @@ export DIB_DISTRIBUTION_MIRROR=http://mirrors.cat.pdx.edu/ubuntu
 
 sudo ls # just to make sure sudo is hot
 
-disk-image-create vm centos -a amd64 -o centos6_x64
+disk-image-create vm ubuntu-minimal -a amd64 -o trusty_x64
 
 mv /var/www/html/diskimages/trusty_x64.qcow2 /var/www/html/diskimages/trusty_x64_1.qcow2
 mv trusty_x64.qcow2 /var/www/html/diskimages/
